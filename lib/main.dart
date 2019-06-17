b2e9a28d8eb9b8e8a22c68291bebe6749cac910f
Sumit Kumar <sumitskl1999@gmail.com>
2019-06-17 18:13:09 +0530

    good

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,41 @@
 import 'package:flutter/material.dart';
 
-void main() {
-    runApp(new DogApp());
-}
-class DogApp extends StatelessWidget {
-    @override
+void main() => runApp(App());
+
+class App extends StatelessWidget {
     Widget build(BuildContext context) {
         return MaterialApp(
-            title: 'My Dog App',
-            home: Scaffold(
-                appBar: AppBar(
-                    centerTitle: true,
-                    title: Text('sbflsdkb'),
-                ),
-            ),
+            home: MainPage(),
         );
     }
+}
+
+class MainPage extends StatefulWidget {
+    _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage>{
+int num=0;
+@override
+Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            centerTitle: true,
+            backgroundColor: Colors.red,
+            title: Text('Eggs Eaten')
+        ),
+        body: Center(
+            child: Text('No. of eggs eaten today: $num'),
+        ),
+        floatingActionButton: FloatingActionButton(
+        onPressed: (){
+            setState(() {
+              num++;
+            });
+        },
+            backgroundColor: Colors.red,
+            child: Icon(Icons.add),
+        ),
+    );
+}
 }
